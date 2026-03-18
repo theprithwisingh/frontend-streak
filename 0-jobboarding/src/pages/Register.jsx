dbc4f186ca26e8fe46225ed085af9fd90c684de4
theprithwisingh <Prithwisingh77@gmail.com>
2026-03-18 21:41:25 +0530

    v2

--- 0-jobboarding/src/pages/Register.jsx
+++ 0-jobboarding/src/pages/Register.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+
+import { z } from "zod";
+const signupSchema = z
+  .object({
+    email: z.string().email(),
+    password: z.string().min(6),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords must match",
+    path: ["confirmPassword"],
+  });
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const { register, handleSubmit } = useForm();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState();
 
-export default Register
+  async function onSubmit() {}
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="text"
+          placeholder="name"
+          {...register("text", { required: true })}
+        />
+        <input
+          type="email"
+          placeholder="email"
+          {...register("email", { required: true })}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          {...register("password", { required: true })}
+        />
+        <input
+          type="password"
+          placeholder="confirmPassword"
+          {...register("confirmPassword", { required: true })}
+        />
+        <button type="submit">submit</button>
+      </form>
+    </>
+  );
+};
+export default Register;
